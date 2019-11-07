6f917a237378948c9f392a39731696ecf4243c4d
Olha <ololoevaevaeva@gmail.com>
2019-11-07 11:31:48 +0200

    envirotech

---
+++ js/common.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    $(".detailed-dropdown-item").click(function () {
+        $(this).closest(".detailed-dropdown-list").find(".detailed-lv1").slideToggle(400).closest(".detailed-dropdown-list").siblings().find(".detailed-lv1").slideUp(400);
+    });
+
+    let heightHeader = $('header').height();
+    $(".detailed, .recruit").css("padding-top", heightHeader);
+    $(".detailed-dropdown").css("top", heightHeader);
+
+    $( ".recruit-datepicker" ).datepicker({ dateFormat: 'dd-mm-yy'}).datepicker("setDate", new Date());
+
+});
